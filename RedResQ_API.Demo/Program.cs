3c38fc49f5bae04a55bd7d6a768dc0cd3ac48f79
Philipp Recknagel <philipp.recknagel@edu.szu.at>
2023-10-06 17:40:05 +0200

    Started working on the API, added Swagger and some demo Maps
    Signed-off-by: Philipp Recknagel <philipp.recknagel@edu.szu.at>

--- RedResQ_API.Demo/Program.cs
+++ RedResQ_API.Demo/Program.cs
@@ -1,6 +1,35 @@
+using Microsoft.OpenApi.Models;
+using RedResQ_API.Lib;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Todo API",
+        Description = "Keep Track of your tasks",
+        Version = "v1"
+    });
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "TODO API V1");
+});
+
+app.MapGet("/", () => "Hello 1!");
+app.MapGet("/test", () => "Hello 2!");
+app.MapPost("/echo", (Product p) =>
+{
+    Product temp = new Product(int.MaxValue - p.Id, p.Name.Reverse().ToString());
+    Console.WriteLine(p.ToString());
+    
+    return new { p.Id, p.Name, temp};
+});
 
 app.Run();
---
+++ RedResQ_API.Lib/SqlHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace RedResQ_API.Lib
+{
+    public class SqlHandler
+    {
+        private SqlConnectionStringBuilder _builder;
+
+        public SqlHandler()
+        {
+            _builder = new SqlConnectionStringBuilder();
+
+        }
+    }
+}
