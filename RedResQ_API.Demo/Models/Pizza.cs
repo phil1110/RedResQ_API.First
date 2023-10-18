ccb27d3a8393161f13af56d61878335ef8e15579
Philipp Recknagel <philipp.recknagel@edu.szu.at>
2023-10-18 13:07:34 +0200

    Fixed some path stuff which was wrong and renaming things

---
+++ RedResQ_API.Demo/Models/Pizza.cs
@@ -0,0 +1,8 @@
+﻿namespace RedResQ_API.Demo.Models;
+
+public class Pizza
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public bool IsGlutenFree { get; set; }
+}
--- RedResQ_API.Lib/SqlHandler.cs
+++ RedResQ_API.Lib/SqlHandler.cs
@@ -5,12 +5,10 @@ namespace RedResQ_API.Lib
 {
     public class SqlHandler
     {
-        private Settings _settings;
         private SqlConnectionStringBuilder _builder;
 
         public SqlHandler()
         {
-            _settings = new Settings();
             _builder = new SqlConnectionStringBuilder();
 
         }
